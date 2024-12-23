c7741c8199fd816f51c61469b126e9c17ff8fc11
kafkiansky <vadimzanfir@gmail.com>
2024-12-23 22:23:18 +0300

    Implement touch

---
+++ Internal/Protocol/Binary/Command/Touch.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Typhoon\Memcached\Internal\Protocol\Binary\Command;
+
+use Typhoon\ByteOrder\WriteTo;
+use Typhoon\Memcached\Expiration;
+use Typhoon\Memcached\Internal\Protocol\Binary\Command;
+use Typhoon\Memcached\Internal\Protocol\Binary\Header;
+use Typhoon\Memcached\Internal\Protocol\Binary\Magic;
+use Typhoon\Memcached\Internal\Protocol\Binary\Opcode;
+use Typhoon\Memcached\Key;
+
+/**
+ * @internal
+ * @psalm-internal Typhoon\Memcached
+ * @template-extends Command<void>
+ */
+final class Touch extends Command
+{
+    /**
+     * @param non-negative-int $id
+     */
+    public function __construct(
+        private readonly int $id,
+        private readonly Key $key,
+        private readonly Expiration $expiration,
+    ) {}
+
+    public function id(): int
+    {
+        return $this->id;
+    }
+
+    public function write(WriteTo $writer): void
+    {
+        $keyValue = (string) $this->key;
+
+        $header = new Header(
+            Magic::REQUEST,
+            Opcode::Touch,
+            $this->id,
+            keyLength: \strlen($keyValue),
+            extrasLength: 4,
+            totalBodyLength: 4 + \strlen($keyValue),
+        );
+
+        $header->write($writer);
+
+        $writer
+            ->writeUint32($this->expiration->value)
+            ->write($keyValue);
+    }
+}
